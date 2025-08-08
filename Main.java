181bffb02e1fd980329b7498ea5afc90d28b4de3
majesty <majestychibuikem@gmail.com>
2025-08-08 10:15:06 +0100

    folder to learn java

---
+++ Main.java
@@ -0,0 +1,12 @@
+public class Main {
+    public static void main(String[] args) {
+        System.out.println("Hello, World!");
+    }
+   
+}
+
+//static
+//main file similarities
+//return types - int, string, bool, double, void
+//javac Main.java - compile
+//java Main  - run the code
